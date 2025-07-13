89d15c7857499508f62d35a73c707cf3d6c3d1bd
Nev <neevass07@gmail.com>
2025-07-13 16:06:03 +0530

    Added Multiple Files

---
+++ Week 4_Spring REST using Spring Boot 3/04_REST - Country Web Service/Code/Country.java
@@ -0,0 +1,35 @@
+package com.cognizant.springlearn;
+
+public class Country {
+    private String code;
+    private String name;
+
+    public Country() {
+        System.out.println(">>> Country constructor called");
+    }
+
+    public String getCode() {
+        System.out.println(">>> getCode()");
+        return code;
+    }
+
+    public void setCode(String code) {
+        System.out.println(">>> setCode()");
+        this.code = code;
+    }
+
+    public String getName() {
+        System.out.println(">>> getName()");
+        return name;
+    }
+
+    public void setName(String name) {
+        System.out.println(">>> setName()");
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" + "code='" + code + '\'' + ", name='" + name + '\'' + '}';
+    }
+}
